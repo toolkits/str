8f379c5bcab5018358d909f6f49f82033c477bc4
Ulric Qin <ulric.qin@gmail.com>
2015-09-21 10:41:27 +0800

    add RandStr func

--- rand.go
+++ rand.go
@@ -23,3 +23,17 @@ func RandSeq(n int) string {
 	return fmt.Sprintf("%d_%s", seed, string(b))
 
 }
+
+func RandStr(n int) string {
+
+	seed := time.Now().UnixNano()
+	rand.Seed(seed)
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(size)]
+	}
+
+	return fmt.Sprintf("%s", string(b))
+
+}
